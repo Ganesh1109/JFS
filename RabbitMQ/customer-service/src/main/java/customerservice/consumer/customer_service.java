3bfe91d8daa0734b2f841af4b64e10162adb9bab
Ganesh1109 <98865646+Ganesh1109@users.noreply.github.com>
2022-05-02 18:26:29 +0530

    Add files via upload

---
+++ RabbitMQ/customer-service/src/main/java/customerservice/consumer/customer_service.java
@@ -0,0 +1,22 @@
+package customerservice.consumer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import customerservice.config.MessagingConfig;
+import customerservice.model.customer;
+import customerservice.model.transactions;
+
+@Component
+public class customer_service {
+    @Autowired
+    private transactions Transactions;
+
+
+    @RabbitListener(queues = MessagingConfig.QUEUE)
+    public void consumeMessageFromQueue(customer c) {
+        System.out.println("Message recieved from queue : " + c);
+        customer customer = Transactions.saveCustomer(c);
+    }
+}
